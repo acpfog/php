a91e73741a69dc00d110df7d438f8fad511444e5
Volodymyr Larkin <acpfog@gmail.com>
2017-04-23 20:52:34 +0200

    Comments added

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Piece of my php code
+# In this repo I store my php scripts
+
+Script `servers_load.php` calculate approx load in percentage for a network interface for each server from a list reading data from rrd files.
+The result looks like a string:
+
+```
+data1:20, data2:21, data3:20, data4:14, data5:22, data6:20
+```
--- servers_load.php
+++ servers_load.php
@@ -1,5 +1,10 @@
 <?php
 
+# set default values for variables
+$result = "";
+$load = 0;
+
+# short and long servers names
 $servers = array(
     "data1" => "host1.server4you.net",
     "data2" => "host2.server4you.net",
@@ -9,25 +14,27 @@ $servers = array(
     "data6" => "host6.server4you.net"
 );
 
-$load = 0;
+# fast ethernet max speed in megabytes per second 
 $max_bytes = 10485760.0;
-$result = "";
 
 foreach ($servers as $name => $server) {
-
+# read data from rrd file using a long server name
     $rrd_data = rrd_fetch( "/var/lib/collectd/rrd/$server/interface-eth0/if_octets.rrd", array( "AVERAGE", "--resolution", "60", "--start", "end-1min" ) );
     $tx = array_values($rrd_data["data"]["tx"])[0]; 
+# if data is absent set load to 1
     if (is_nan($tx)) {
         $load = 1;
     } else {
+# calculate and round a server load
         $load = round( ( $tx / $max_bytes ) * 100 );
+# normal load value sould be in range from 1 to 99 
         if ( $load <= 0 ) {
             $load = 1;
         } elseif ( $load > 99 ) {
             $load = 99;
         }
     }
-
+# check if http service is alive
     $http = curl_init("http://$server");
     curl_setopt($http, CURLOPT_HEADER, true);
     curl_setopt($http, CURLOPT_NOBODY, true);
@@ -36,15 +43,15 @@ foreach ($servers as $name => $server) {
     $response = curl_exec($http);
     $httpcode = curl_getinfo($http, CURLINFO_HTTP_CODE);
     curl_close($http);
-
+# if http service isn't alive set load to 100
     if ( $httpcode != "200" ) {
         $load = 100;
     }
-
+# add a string to the result
     $result .= "$name:$load, ";
 
 }
-
+# print the result string
 echo rtrim($result,", ");
 
 ?>
